068bea21ec5bc393192014261fec85c80638fc85
Davi Medeiros <davi.med.oli1303@gmail.com>
2024-05-18 00:37:50 -0300

    created spring gradle project

---
+++ automated_qa/src/main/java/br/com/qa/automated/automated_qa/AutomatedQaApplication.java
@@ -0,0 +1,13 @@
+package br.com.qa.automated.automated_qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedQaApplication.class, args);
+	}
+
+}
---
+++ automated_qa/src/test/java/br/com/qa/automated/automated_qa/AutomatedQaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.qa.automated.automated_qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
